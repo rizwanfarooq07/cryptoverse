df97604b8e6a8430d3b4e4d946dac1d48863bb93
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-10-20 16:25:17 +0530

    updated readMe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">Cryptoverse</Link>
+          <Link to="/">CryptoPlanet</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
